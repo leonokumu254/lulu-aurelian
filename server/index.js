6be08531e16e15e55930d29634796f72bcdf6fcb
leonokumu254 <leonshireku@gmail.com>
2026-08-22 08:42:04 +0300

    feat: initialize Express server with security, routing, and cron service integration

--- server/index.js
+++ server/index.js
@@ -49,7 +49,7 @@ const allowedOrigins = [
   'https://lulu-aurelian.vercel.app',
   'http://localhost:5173',
   'http://localhost:5000',
-  'http://localhost:3000'
+  'http://localhost:3000' 
 ];
 
 app.use(cors({
